f23633ead9c253b80d9d78aad6f0bae787df1e56
akiva <akiva.grobman@gmail.com>
2020-10-09 09:48:25 +0300

    template reuse and static resources

--- src/main/java/com/akiva/Main.java
+++ src/main/java/com/akiva/Main.java
@@ -6,12 +6,13 @@ import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.util.HashMap;
 import java.util.Map;
-import static spark.Spark.get;
-import static spark.Spark.post;
+
+import static spark.Spark.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        staticFileLocation("/public");
         CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
         get("/", (req, res) -> {
                  Map<String, String> model = new HashMap<>();
